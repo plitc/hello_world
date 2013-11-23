8ee1da3b1249f4b274954a02e58c7732c4e58a4a
Daniel Plominski <Service@PLITC.eu>
2013-11-23 03:13:35 +0100

    third commit

---
+++ hello_world.c
@@ -0,0 +1,16 @@
+/* hello_world.c -- hello, world! */
+ 
+#include<stdio.h>
+ 
+        int main()
+        {
+                int i;
+                for (i = 0; i < 1000000; i++)
+                {
+                        printf ("Hello\n");
+                        printf ("World\n");
+                }
+        return 0;
+        }
+
+/* EOF */
